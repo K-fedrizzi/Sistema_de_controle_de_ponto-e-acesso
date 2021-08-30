15f597cdd3e2e6963800da5b541c2f0a674c1a4f
Karina Fedrizzi <karina.fedrizzi91@gmail.com>
2021-08-29 22:11:41 -0300

    CRUD da classe JornadaTrabalho criado

---
+++ src/main/java/com/dio/live/controller/JornadaTrabalhoController.java
@@ -0,0 +1,36 @@
+package com.dio.live.controller;
+
+
+import com.dio.live.service.JornadaService;
+import com.dio.live.model.JornadaTrabalho;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/jornada")
+public class JornadaTrabalhoController {
+    @Autowired
+    JornadaService jornadaService;
+
+    @PostMapping
+    public JornadaTrabalho createJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.saveJornada(jornadaTrabalho);
+
+    }
+
+   @GetMapping
+    public List<JornadaTrabalho> getJornadaList(){
+     return jornadaService.findAll();
+    }
+
+    @GetMapping("/{idJornada}")
+    public ResponseEntity<JornadaTrabalho> getJornadaByID(@PathVariable("idJornada") long idJornada) throws Exception{
+        return ResponseEntity.ok(jornadaService.getByid(idJornada).orElseThrow( ()-> new NoSuchElementException("Not Found!")));
+
+    }
+}
